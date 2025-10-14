0b04a7c3d805474495c54e2fe818c2714cae34a8
Fiore Mario Vitale <mvitale@redhat.com>
2025-10-14 17:31:29 +0200

    DBZ-9541 Revisit OpenLineage integration in a way that the runId is now in the CdcSourceTaskContext and shared with the OpenLineage SMT via record headers

--- debezium-connector-binlog/src/main/java/io/debezium/connector/binlog/BinlogDatabaseSchema.java
+++ debezium-connector-binlog/src/main/java/io/debezium/connector/binlog/BinlogDatabaseSchema.java
@@ -16,6 +16,8 @@ import java.util.concurrent.ConcurrentHashMap;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
+import io.debezium.config.CommonConnectorConfig;
+import io.debezium.connector.common.CdcSourceTaskContext;
 import io.debezium.relational.CustomConverterRegistry;
 import io.debezium.relational.DefaultValueConverter;
 import io.debezium.relational.HistorizedRelationalDatabaseSchema;
@@ -77,7 +79,8 @@ public abstract class BinlogDatabaseSchema<P extends BinlogPartition, O extends 
                                 D defaultValueConverter,
                                 TopicNamingStrategy<TableId> topicNamingStrategy,
                                 SchemaNameAdjuster schemaNameAdjuster,
-                                boolean tableIdCaseInsensitive, CustomConverterRegistry converterRegistry) {
+                                boolean tableIdCaseInsensitive, CustomConverterRegistry converterRegistry,
+                                CdcSourceTaskContext<? extends CommonConnectorConfig> taskContext) {
         super(connectorConfig,
                 topicNamingStrategy,
                 connectorConfig.getTableFilters().dataCollectionFilter(),
@@ -92,7 +95,7 @@ public abstract class BinlogDatabaseSchema<P extends BinlogPartition, O extends 
                         false,
                         connectorConfig.getEventConvertingFailureHandlingMode()),
                 tableIdCaseInsensitive,
-                connectorConfig.getKeyMapper());
+                connectorConfig.getKeyMapper(), taskContext);
         this.ddlParser = createDdlParser(connectorConfig, valueConverter);
         this.ddlChanges = this.ddlParser.getDdlChanges();
         this.connectorConfig = connectorConfig;
